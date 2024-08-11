973a39527330c7807cb7eb34da46dacebbe14f54
unknown <zhanel833.@gmail.com>
2024-08-11 17:02:35 +0500

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+//        boolean helloworld = bean == bean2;
+        System.out.println(bean == bean2);
+//        boolean catt= cat == cat2;
+        System.out.println(cat == cat2);
+
+
     }
 }
